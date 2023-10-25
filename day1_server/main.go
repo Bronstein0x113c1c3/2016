fb4a7d5f94214672a6f7f363f42c1a0ffd281ee3
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-10-25 20:51:53 +0700

    hmm, i found many things today...

--- day1_server/main.go
+++ day1_server/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"net"
 	"os"
 	"os/signal"
+	"sync"
 )
 
 // func handleConnection(conn net.Conn) {
@@ -34,23 +36,42 @@ import (
 // 	}
 // }
 
-func handleConnection(conn net.Conn) {
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
-	go func(ctx context.Context) {
+// func handleConnection(conn net.Conn) {
+// 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+// 	defer cancel()
+// 	go func(ctx context.Context) {
+// 		select {
+// 		case <-ctx.Done():
+// 			fmt.Println("Done!")
+// 			conn.Write([]byte("Goodbye! \n"))
+// 			conn.Close()
+// 			cancel()
+// 			return
+// 		default:
+// 			for {
+// 				conn.Write([]byte("Hello!"))
+// 			}
+// 		}
+// 	}(ctx)
+
+// }
+func handleConnection(conn net.Conn, ctx context.Context, wg *sync.WaitGroup) {
+	defer func() {
+		wg.Done()
+		log.Printf("Closing: %v", conn.RemoteAddr())
+	}()
+	for {
 		select {
 		case <-ctx.Done():
-			fmt.Println("Done!")
-			conn.Write([]byte("Goodbye! \n"))
-			conn.Close()
-			cancel()
+			fmt.Fprintln(conn, "Goodbye")
+			log.Printf("%v: Goodbye! \n", conn.RemoteAddr())
 			return
 		default:
-			for {
-				conn.Write([]byte("Hello!"))
-			}
+			log.Println("Hello")
+			fmt.Fprintln(conn, "Hello")
 		}
-	}(ctx)
+	}
+	// fmt.Fprintln(conn, "Hello...")
 
 }
 func main() {
@@ -60,7 +81,19 @@ func main() {
 		fmt.Println("Error setting up TCP listener:", err)
 		os.Exit(1)
 	}
-
+	//step1: setup the context,waitgroup
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	wg := &sync.WaitGroup{}
+	defer cancel()
+	log.Println("Done setting up")
+	/*
+		- setup context from signal with the main, listener
+		- derive the main context to each smaller connection (maybe uneccessary, but with good impact....)
+		- setup to get the signal
+	*/
+	go func() {
+		<-ctx.Done()
+	}()
 	// Accept connections in a loop
 	for {
 		conn, err := listener.Accept()
@@ -68,7 +101,16 @@ func main() {
 			fmt.Println("Error accepting connection:", err)
 			break
 		}
-
-		go handleConnection(conn)
+		wg.Add(1)
+		childctx, _ := context.WithCancel(ctx)
+		go handleConnection(conn, childctx, wg)
+		select {
+		case <-ctx.Done():
+			log.Println("Starting closing down")
+			wg.Wait()
+			listener.Close()
+			log.Println("Server closed")
+			os.Exit(0)
+		}
 	}
 }
