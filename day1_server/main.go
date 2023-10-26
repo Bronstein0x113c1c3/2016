703e650f7d36fe6bce840e7b7e5118550eeeb85d
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-10-26 19:47:54 +0700

    new approach with listener used as channel :)

--- day1_server/main.go
+++ day1_server/main.go
@@ -56,15 +56,13 @@ import (
 
 // }
 func handleConnection(conn net.Conn, ctx context.Context, wg *sync.WaitGroup) {
-	defer func() {
-		wg.Done()
-		log.Printf("Closing: %v", conn.RemoteAddr())
-	}()
 	for {
 		select {
 		case <-ctx.Done():
 			fmt.Fprintln(conn, "Goodbye")
 			log.Printf("%v: Goodbye! \n", conn.RemoteAddr())
+			conn.Close()
+			wg.Done()
 			return
 		default:
 			log.Println("Hello")
@@ -74,43 +72,102 @@ func handleConnection(conn net.Conn, ctx context.Context, wg *sync.WaitGroup) {
 	// fmt.Fprintln(conn, "Hello...")
 
 }
+
+// func main() {
+// 	// Set up a TCP listener
+// 	listener, err := net.Listen("tcp", ":3000")
+// 	if err != nil {
+// 		fmt.Println("Error setting up TCP listener:", err)
+// 		os.Exit(1)
+// 	}
+// 	//step1: setup the context,waitgroup
+// 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+// 	wg := &sync.WaitGroup{}
+// 	defer cancel()
+// 	conn_chan := make(chan net.Conn)
+// 	signal := make(chan bool)
+// 	/*
+// 		- setup context from signal with the main, listener
+// 		- derive the main context to each smaller connection (maybe uneccessary, but with good impact....)
+// 		- setup to get the signal
+// 	*/
+
+// 	// Accept connections in a loop
+// 	wg.Add(1)
+// 	go func(wg *sync.WaitGroup, ctx context.Context, s chan bool) {
+// 		for {
+// 			select {
+// 			case <-s:
+// 				wg.Done()
+// 				return
+// 			default:
+// 				conn, err := listener.Accept()
+// 				if err != nil {
+// 					fmt.Println("Error accepting connection:", err)
+// 					break
+// 				}
+// 				conn_chan <- conn
+// 			}
+// 		}
+
+// 	}(wg, ctx, signal)
+// 	for {
+// 		select {
+// 		case <-ctx.Done():
+// 			signal <- true
+// 			goto ending
+// 		case conn := <-conn_chan:
+// 			wg.Add(1)
+// 			go handleConnection(conn, ctx, wg)
+// 		}
+// 	}
+// ending:
+// 	log.Println("Waiting for ....")
+// 	wg.Wait()
+// 	listener.Close()
+// 	log.Println("closed listener...")
+// 	os.Exit(0)
+
+// }
+
+//another approach....
+
+func listening(listener net.Listener) <-chan net.Conn {
+	res := make(chan net.Conn)
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error when accepting...: ", err)
+			}
+			res <- conn
+		}
+	}()
+	return res
+}
+
 func main() {
-	// Set up a TCP listener
+	defer log.Println("Closed listener....")
 	listener, err := net.Listen("tcp", ":3000")
+	defer listener.Close()
 	if err != nil {
-		fmt.Println("Error setting up TCP listener:", err)
-		os.Exit(1)
+		log.Fatalln("Error when creating listener: ", err)
 	}
-	//step1: setup the context,waitgroup
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	wg := &sync.WaitGroup{}
+	conns := listening(listener)
 	defer cancel()
-	log.Println("Done setting up")
-	/*
-		- setup context from signal with the main, listener
-		- derive the main context to each smaller connection (maybe uneccessary, but with good impact....)
-		- setup to get the signal
-	*/
-	go func() {
-		<-ctx.Done()
-	}()
-	// Accept connections in a loop
+	log.Println("Done preparing....")
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection:", err)
-			break
-		}
-		wg.Add(1)
-		childctx, _ := context.WithCancel(ctx)
-		go handleConnection(conn, childctx, wg)
 		select {
+		case conn := <-conns:
+			wg.Add(1)
+			go handleConnection(conn, ctx, wg)
 		case <-ctx.Done():
-			log.Println("Starting closing down")
+			log.Println("Waiting for ....")
 			wg.Wait()
-			listener.Close()
-			log.Println("Server closed")
-			os.Exit(0)
+			log.Println("closing listener...")
+			return
 		}
 	}
 }
