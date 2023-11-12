148ddb9e743596736294841913c7eb3004bed4d1
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2023-11-13 00:58:13 +0700

    something new....

--- day5_template/main.go
+++ day5_template/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"log"
 	"os"
 	"text/template"
 )
 
 func main() {
-	t := template.New("Bonjour")
-	template_string := "Hello, {{.Name}} \n"
-	t.Parse(template_string)
-	t.Execute(os.Stdout, struct {
+	t, err := template.ParseFiles("template.txt")
+	if err != nil {
+		log.Println("Some problem in creating template.....")
+		os.Exit(1)
+	}
+	lists := []struct {
 		Name string
-	}{"Jonathan"})
+		ID   string
+	}{
+		{
+			Name: "Bronstein",
+			ID:   "1624640",
+		},
+		{
+			Name: "Vladimir",
+			ID:   "20218105",
+		},
+	}
+	if err = t.ExecuteTemplate(os.Stdout, "template.txt", struct {
+		List []struct {
+			Name string
+			ID   string
+		}
+	}{lists}); err != nil {
+		log.Println("Cannot parse.....")
+		os.Exit(1)
+	}
+
 }
