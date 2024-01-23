9f790a7ffa7ff360d389d7b2c6decd4b99fd295c
Bronstein0x113c1c3 <tuananhsmart123456789@gmail.com>
2024-01-24 00:16:30 +0700

    an example for real thangs....

---
+++ day19_realthings/upload.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func HandleMultipartForm(writer http.ResponseWriter, request *http.Request) {
+	request.ParseMultipartForm(10000000)
+	fmt.Fprintf(writer, "Name: %v, City: %v\n",
+		request.MultipartForm.Value["name"][0],
+		request.MultipartForm.Value["city"][0])
+	fmt.Fprintln(writer, "------")
+	for _, header := range request.MultipartForm.File["files"] {
+		fmt.Fprintf(writer, "Name: %v, Size: %v\n", header.Filename, header.Size)
+		file, err := header.Open()
+		if err == nil {
+			defer file.Close()
+			fmt.Fprintln(writer, "------")
+			io.Copy(writer, file)
+		} else {
+			http.Error(writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+func init() {
+	http.HandleFunc("/forms/upload", HandleMultipartForm)
+}
